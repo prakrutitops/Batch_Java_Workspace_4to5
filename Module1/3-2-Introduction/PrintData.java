51b27364fbcf580c315ce19e02df142ade49b9c0
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-02-03 17:02:22 +0530

    Add files via upload

---
+++ Module1/3-2-Introduction/PrintData.java
@@ -0,0 +1,29 @@
+package com.a32;
+
+public class PrintData 
+{
+	public static void main(String[] args) 
+	{
+	
+		//Diffrence between print and println
+		
+	/*	gh
+		h
+		gfhf
+		gh
+		f
+		hgf
+		hf
+		hgf
+		*/
+		
+		
+		
+		
+		System.out.println("Java");
+		System.out.println("Android");
+		System.out.print("Flutter");
+		System.out.print("Php");
+		
+	}
+}
